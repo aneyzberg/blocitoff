00c601c84f9e82668374575ce01c9827ffba0646
Anna Neyzberg <aneyzberg@gmail.com>
2014-07-07 03:15:35 +0000

    Created welcome controller with index and about views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Blocitoff::Application.routes.draw do
+  get "welcome/index"
+  get "welcome/about"
+
+  root to: 'welcome#index'
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +58,4 @@ Blocitoff::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
